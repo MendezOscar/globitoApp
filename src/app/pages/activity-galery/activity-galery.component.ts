742372bdb7e4defa142f4fca9868b7ad6da939b2
Oscar Cruz <ocruz@caracolknits.com>
2020-05-22 08:54:14 -0600

    Login br

---
+++ src/app/pages/activity-galery/activity-galery.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-activity-galery',
+  templateUrl: './activity-galery.component.html',
+  styleUrls: ['./activity-galery.component.scss'],
+})
+export class ActivityGaleryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
